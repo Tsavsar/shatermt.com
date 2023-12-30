71a32863e12950d823018de2bc322071d1b28274
Tsavsar <shatermt@gmail.com>
2023-12-30 13:02:03 +0100

    aadded analytics

--- astro.config.ts
+++ astro.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'astro/config';
-import UnoCSS from 'unocss/astro'
-import svelte from '@astrojs/svelte';
+import { defineConfig } from "astro/config";
+import UnoCSS from "unocss/astro";
+import svelte from "@astrojs/svelte";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    UnoCSS({injectReset: true}),
-  svelte(), 
-  ],});
+  integrations: [UnoCSS({ injectReset: true }), svelte()],
+  output: 'server',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  })
+});
