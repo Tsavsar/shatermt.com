e75b21942a21381a9cea0301386a2bd44fd2f513
Haniel Ubogu <hanieltobi@gmail.com>
2024-01-02 06:10:09 +0000

    use correct site

--- astro.config.ts
+++ astro.config.ts
@@ -8,7 +8,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://example.com",
+  site: "https://shatermt.com",
   integrations: [UnoCSS({ injectReset: true }), svelte(), mdx(), sitemap()],
   adapter: vercel({
     webAnalytics: {
